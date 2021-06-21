25d9ca493aa9fbf48705dc4ba4c2cf720ba5f1f1
Deviant <michaelngo1337@yahoo.com>
2021-06-21 09:51:25 -0700

    created user routes layout

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,3 +1,42 @@
 const router = require('express').Router()
-const {Post, Comment} = require('../models')
+const {User, Post, Comment} = require('../models')
 
+//get all users
+router.get('/users', (req, res) => {
+  User.findAll({
+    include: [
+      { model: Post },
+      { model: Comment }
+    ]
+  })
+  .then(users => res.json(users))
+  .catch(err => console.log(err))
+})
+
+//get one user by their id
+router.get('/users/:id', (req, res) => {
+  User.findOne({
+    where: {id: req.params.id},
+    include: [
+      { model: Post },
+      { model: Comment }
+    ]
+  })
+  .then(user => res.json(user))
+  .catch(err => console.log(err))
+})
+
+// create new user
+router.post('/users', (req, res) => {
+
+})
+
+// update existing user by id
+router.put('/users/:id', (req, res) => {
+
+})
+
+// delete existing user by id
+router.delete('/users/:id', (req, res) => {
+
+})
