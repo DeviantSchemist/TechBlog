670e05db8d2061360a4107a96a563e566b9b9d52
Deviant <michaelngo1337@yahoo.com>
2021-06-20 21:22:41 -0700

    implemented error check in index controller file and a little bit of code in userRoutes.js

--- controllers/index.js
+++ controllers/index.js
@@ -4,4 +4,9 @@ router.use('/api', require('./userRoutes'))
 router.use('/api', require('./commentRoutes'))
 router.use('/api', require('./postRoutes'))
 
+// send this to user if route is incorrect
+router.use((req, res) => {
+  res.send('<h1>Wrong Route!</h1>')
+})
+
 module.exports = router
