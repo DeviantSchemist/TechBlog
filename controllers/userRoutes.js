017026748dd1b981f91047e622cc4454d9aa5024
Deviant <michaelngo1337@yahoo.com>
2021-06-21 21:11:17 -0700

    require express handlebars in server.js and implemented User model

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const {User, Post, Comment} = require('../models')
+const jwt = require('jsonwebtoken')
 
 //get all users
 router.get('/users', (req, res) => {
@@ -28,15 +29,40 @@ router.get('/users/:id', (req, res) => {
 
 // create new user
 router.post('/users', (req, res) => {
-
+  User.create(req.body)
+  .then(user => res.json(user))
+  .catch(err => console.log(err))
 })
 
 // update existing user by id
 router.put('/users/:id', (req, res) => {
-
+  User.update(req.body, { where: { id: req.params.id } })
+  .then(() => res.sendStatus(200))
+  .catch(err => console.log(err))
 })
 
 // delete existing user by id
 router.delete('/users/:id', (req, res) => {
-
+  User.destroy({ where: {id: req.params.id } })
+  .then(() => res.sendStatus(200))
+  .catch(err => console.log(err))
 })
+
+// route for new users registering for site
+router.post('/users/register', (req, res) => {
+  const { name, email, username } = req.body
+  User.register(new User({ name, email, username }), req.body.password, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
+})
+
+// route for existing users logging in
+router.post('/users/login', (req, res) => {
+  User.authenticate()(req.body.username, req.body.password, (err, user) => {
+    if (err) { console.log(err) }
+    res.json(user ? jwt.sign({ id: user.id }, process.env.SECRET) : null)
+  })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const handlebars = require('express-handlebars')
 const { join } = require('path')
 const passport = require('passport')
 const { User } = require('./models')
