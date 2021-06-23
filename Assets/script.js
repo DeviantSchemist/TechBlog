45275f7bfb81f9ee8456126f95b56862acb07fb5
Deviant <michaelngo1337@yahoo.com>
2021-06-22 19:24:13 -0700

    added login and register features to script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,32 +1,28 @@
-const { axios } = require('axios')
-
-//This is a login skeleton
-document.getElementById('name of login button').addEventListener('click', event => {
+// logs in user
+document.getElementById('login').addEventListener('click', event => {
   event.preventDefault()
   axios.post('/api/users/login', {
     username: document.getElementById('username').value,
     password: document.getElementById('password').value
   })
-    .then(({ data: token }) => {
-      if (token) {
-        localStorage.setItem('token', token)
-        window.location = 'name of html page'
-      } else {
-        alert('Invalid username or password')
-      }
-    })
-    .catch(err => console.error(err))
+  .then(({ data: token }) => {
+    if (token) {
+      localStorage.setItem('token', token)
+      window.location = '/index.html'
+    } else {
+      alert('Invalid username or password')
+    }
+  })
+  .catch(err => console.error(err))
 })
 
-//This is a register skeleton
-document.getElementById('name of register button').addEventListener('click', event => {
+// registers new user
+document.getElementById('register').addEventListener('click', event => {
   event.preventDefault()
   axios.post('/api/users/register', {
-    name: document.getElementById('name').value,
-    email: document.getElementById('email').value,
     username: document.getElementById('username').value,
     password: document.getElementById('password').value
   })
-    .then(() => window.location = 'name of html page')
-    .catch(err => console.error(err))
+  .then(() => window.location = '/login.html')
+  .catch(err => console.error(err))
 })
