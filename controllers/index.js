343b0ce030cb4e0ea0e13366a239012b4e9ef1b8
Deviant <michaelngo1337@yahoo.com>
2021-06-22 21:10:34 -0700

    commented out in router index.js file

--- controllers/index.js
+++ controllers/index.js
@@ -5,8 +5,8 @@ router.use('/api', require('./commentRoutes.js'))
 router.use('/api', require('./postRoutes.js'))
 
 //send this to user if route is incorrect
-router.use((req, res) => {
-  res.send('<h1>Wrong Route!</h1>')
-})
+// router.use((req, res) => {
+//   res.send('<h1>Wrong Route!</h1>')
+// })
 
 module.exports = router
