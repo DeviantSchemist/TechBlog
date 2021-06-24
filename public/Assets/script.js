74a5b066d23b087dd587e35479081f2b77b6e4a2
Deviant <michaelngo1337@yahoo.com>
2021-06-24 01:57:38 -0700

    updated html pages with nav links, updated js files

--- public/Assets/script.js
+++ public/Assets/script.js
@@ -9,3 +9,10 @@ document.getElementById('logoutNav').addEventListener('click', () => {
   localStorage.removeItem('token')
   window.location = '/index.html'
 })
+
+document.getElementById('postButton').addEventListener('click', event => {
+  if (document.getElementById('createPost').classList.contains('disappear')) {
+    document.getElementById('createPost').classList.remove('disappear')
+    event.target.classList.add('disappear')
+  }
+})
