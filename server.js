d008107ee8ede6f2c1bbb2f72f386e4380366f25
Deviant <michaelngo1337@yahoo.com>
2021-06-24 20:33:49 -0700

    added code to handlebars files

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-// app.engine('handlebars', handlebars())
+// app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 // app.set('view engine', 'handlebars')
 // app.get('/', (req, res) => {res.render('index')})
 
